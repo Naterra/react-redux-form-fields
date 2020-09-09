b496af6e282cd79bda49185ee46d0331556e17f3
Naterra <natalinyweb@hotmail.com>
2020-09-09 00:54:09 -0400

    Input field story

---
+++ stories/Input.stories.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Input from '../src/Input';
+
+export default {
+	title: 'Forms/Input',
+	component: Input,
+	argTypes: {}
+};
+
+const Template = args => <Input {...args} />;
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+	label: 'Full Name',
+	placeholder: 'Jon Snow'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	...Primary.args,
+    disabled: true,
+};
+
+export const Required = Template.bind({});
+Required.args = {
+	...Primary.args,
+	required: true,
+};
+
+export const ErrorMessage = Template.bind({});
+ErrorMessage.args = {
+	...Primary.args,
+	meta: {
+		touched:true,
+		error:'Field cannot be empty'
+	},
+};
+
+export const WarningMessage = Template.bind({});
+WarningMessage.args = {
+	...Primary.args,
+	meta: {
+		touched:true,
+		warning:'Too short'
+	},
+};
