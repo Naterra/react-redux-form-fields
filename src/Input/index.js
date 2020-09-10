7e2324b5ed93d4dd35470ce119b7db65e2dc796c
Naterra <natalinyweb@hotmail.com>
2020-09-09 23:04:07 -0400

    Input field validation

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import '../assets/styles.css';
 
-const Input = ({ input, type='text', label = '', preLabel, placeholder = '', required, disabled = false, meta: { touched, error, warning } }) => (
-	<div className={`input-field ${required ? 'required' : ''}`}>
-		{preLabel && <span style={{}}>{preLabel || ''}</span>}
+const Input = ({ input, type = 'text', label = '', preLabel, placeholder = '', required, disabled = false, meta: { touched, error, warning } }) => {
+	const validate = ['email'].includes(type);
 
-		<input {...input} type={type} placeholder={placeholder} disabled={disabled} autoComplete="off" />
-		<label className="active" htmlFor={input.name}>
-			{label}
-		</label>
+	return (
+		<div className={`input-field ${required ? 'required' : ''}`}>
+			{preLabel && <span style={{}}>{preLabel || ''}</span>}
 
-		{touched &&
-			(error || warning) && (
-				<div className="form-field-error">
-					{error && <span className="field-error">{error}</span>}
-					{warning && <span className="field-warn">{warning}</span>}
-				</div>
-			)}
-	</div>
-);
+			<input {...input} type={type} placeholder={placeholder} disabled={disabled} autoComplete="off" className={`${validate ? 'validate' : ''}`} />
+			<label className="active" htmlFor={input.name}>
+				{label}
+			</label>
+
+			{validate && <span className="helper-text" data-error="wrong" data-success="right" />}
+
+			{touched &&
+				(error || warning) && (
+					<div className="form-field-error">
+						{error && <span className="field-error">{error}</span>}
+						{warning && <span className="field-warn">{warning}</span>}
+					</div>
+				)}
+		</div>
+	);
+};
 
 Input.defaultProps = {
 	input: {},
