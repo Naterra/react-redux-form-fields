f26f9d0319cfa2c9b85a0ceffc3f05dcbe963ec4
Naterra <natalinyweb@hotmail.com>
2020-09-11 08:46:44 -0400

    ImageField component and story

---
+++ src/ImageField/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const ImageField = ({ input, name, label = 'File', fieldName, formValues, deleteImageFile }) => {
+	const { value, ...inputProps } = input;
+
+	const initialValue = (formValues && formValues[name]) || value;
+
+	const handleChange = e => {
+		const file = e.target.files[0];
+		input.onChange(file);
+	};
+
+	if (initialValue) {
+		return (
+			<div className="col s12">
+				<label style={{ float: 'left', display: 'block', width: '100%', marginBottom: '10px' }}>{label}</label>
+				<img src={initialValue} style={{ width: '200px' }} />
+				<i className="material-icons red-text" onClick={e => (deleteImageFile ? deleteImageFile(name) : false)}>
+					delete_forever
+				</i>
+			</div>
+		);
+	} else {
+		return <UploadInterface label={label} inputProps={inputProps} handleChange={handleChange} />;
+	}
+};
+
+ImageField.defaultProps = {
+	label: 'File',
+	name: 'file',
+	deleteImageFile: null
+};
+
+const UploadInterface = ({ label, inputProps, handleChange }) => {
+	return (
+		<div className="file-field input-field">
+			<div className="btn">
+				<span>{label}</span>
+				<input {...inputProps} type="file" onChange={e => handleChange(e)} />
+			</div>
+			<div className="file-path-wrapper">
+				<input className="file-path validate" type="text" />
+			</div>
+		</div>
+	);
+};
+
+export default ImageField;
---
+++ stories/ImageField.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ImageField from '../src/ImageField';
+import { Field } from 'redux-form';
+import ReduxForm from '../.storybook/decorators/ReduxForm';
+
+export default {
+	title: 'ImageField',
+	component: ImageField,
+	argTypes: {},
+	decorators: [story => <ReduxForm story={story} />]
+};
+
+const Template = args => <Field name="File" component={ImageField} {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+	label: 'File',
+	name: 'file'
+};
+
+export const DisplayFile = Template.bind({});
+DisplayFile.args = {
+	...Primary.args,
+	input: {
+		value: 'https://www.nymetroparents.com/columnpic2/manhattan-skyline-sunset.jpg'
+	}
+};
